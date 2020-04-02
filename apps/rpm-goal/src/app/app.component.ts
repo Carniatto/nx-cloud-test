47d5e766ac737507a3d41fbc0600ae1aebc6928c
carniatto <mateus.carniato@realimpactanalytics.com>
2020-04-02 09:57:50 +0300

    added prettier

--- apps/rpm-goal/src/app/app.component.ts
+++ apps/rpm-goal/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Message } from '@nx-cloud-test/api-interfaces';
 @Component({
   selector: 'nx-cloud-test-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   hello$ = this.http.get<Message>('/api/hello');
